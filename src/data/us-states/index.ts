5b2c791645199435947fde37455b159ea7455228
Yuta <yuta256dev@gmail.com>
2026-04-01 17:09:19 +0900

    feat: アメリカの英語の州名モードを追加

--- src/data/us-states/index.ts
+++ src/data/us-states/index.ts
@@ -4,6 +4,7 @@ import states from "@/data/us-states/states.json";
 export interface USState {
   id: string;
   name: string;
+  nameNative: string;
   capital: string;
   flag: string;
 }
@@ -15,11 +16,13 @@ export const usStatesMode: GameModeConfig = {
   hasAreaSelection: false,
   questionOptions: [
     { labelKey: "content-type.state-name", value: "name" },
+    { labelKey: "content-type.us-state-nameNative", value: "nameNative" },
     { labelKey: "content-type.state-capital", value: "capital" },
     { labelKey: "content-type.state-flag", value: "flag" },
   ],
   answerOptions: [
     { labelKey: "content-type.state-name", value: "name" },
+    { labelKey: "content-type.us-state-nameNative", value: "nameNative" },
     { labelKey: "content-type.state-capital", value: "capital" },
     { labelKey: "content-type.state-flag", value: "flag" },
   ],
@@ -28,6 +31,7 @@ export const usStatesMode: GameModeConfig = {
   fieldDisplayTypes: {
     id: "id",
     name: "text",
+    nameNative: "text",
     capital: "text",
     flag: "img",
   },
@@ -35,17 +39,18 @@ export const usStatesMode: GameModeConfig = {
     states.map((state) => ({
       id: state.code,
       name: state.name,
+      nameNative: state.nameNative,
       capital: state.capital,
       flag: `https://flagcdn.com/${state.code}.svg`,
     })),
   normalizeQuestionField: (value) => {
-    if (value === "capital" || value === "flag") {
+    if (value === "capital" || value === "nameNative" || value === "flag") {
       return value;
     }
     return "name";
   },
   normalizeAnswerField: (value) => {
-    if (value === "name" || value === "capital") {
+    if (value === "name" || value === "nameNative" || value === "capital") {
       return value;
     }
     return "flag";
